04361b8dd7df51c09859217dbbf8827fc69162a5
Kacper <112724545+AccrJpg@users.noreply.github.com>
2023-01-08 20:12:28 +0100

    Add files via upload

---
+++ src/components/AddPost.js
@@ -0,0 +1,43 @@
+import './AddPost.css';
+
+import { useState } from 'react';
+
+
+import axios from 'axios';
+
+const AddPost = (props) => {
+
+    const [postContent, setPostContent] = useState('');
+
+    const addPost = (e) => {
+        e.preventDefault();
+
+        if (!postContent) {
+            return;
+        }
+
+        axios
+            .post("https://akademia108.pl/api/social-app/post/add", {
+                content: postContent,
+            })
+
+            .then((res) => {
+                props.getPrevPosts();
+                setPostContent('')
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+
+
+    return (
+        <form className='addPostform' onSubmit={addPost}>
+            <textarea placeholder="Add post..." onChange={(e) => setPostContent(e.target.value)} value={postContent} ></textarea>
+            <button className="btn">Add</button>
+        </form>
+    );
+};
+
+export default AddPost
